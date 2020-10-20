301a867cd45fefd73f7f2b56d65960314734e45d
Johan Fernando Romo Eligio <feryohan343@gmail.com>
2020-10-19 22:47:05 -0500

    Programa con gráfica

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtBoxPromedio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxMin = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxMax = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -58,7 +66,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(464, 61);
+            this.label2.Location = new System.Drawing.Point(463, 66);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(270, 16);
             this.label2.TabIndex = 2;
@@ -67,17 +75,70 @@
             // txtBoxPromedio
             // 
             this.txtBoxPromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBoxPromedio.Location = new System.Drawing.Point(467, 89);
+            this.txtBoxPromedio.Location = new System.Drawing.Point(466, 94);
             this.txtBoxPromedio.Name = "txtBoxPromedio";
             this.txtBoxPromedio.Size = new System.Drawing.Size(288, 22);
             this.txtBoxPromedio.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(463, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor mínimo";
+            // 
+            // txtBoxMin
+            // 
+            this.txtBoxMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxMin.Location = new System.Drawing.Point(466, 163);
+            this.txtBoxMin.Name = "txtBoxMin";
+            this.txtBoxMin.Size = new System.Drawing.Size(288, 22);
+            this.txtBoxMin.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(463, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Valor Máximo";
+            // 
+            // txtBoxMax
+            // 
+            this.txtBoxMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxMax.Location = new System.Drawing.Point(466, 232);
+            this.txtBoxMax.Name = "txtBoxMax";
+            this.txtBoxMax.Size = new System.Drawing.Size(288, 22);
+            this.txtBoxMax.TabIndex = 3;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(466, 310);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(739, 392);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Khaki;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1217, 714);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.txtBoxMax);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxMin);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.txtBoxPromedio);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -87,6 +148,7 @@
             this.Text = "Lanzamientos Espaciales Exitosos";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,6 +160,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtBoxPromedio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxMin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxMax;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//Libreria para el chart
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Lanzamientos_Espaciales_Exitosos
 {
@@ -29,6 +31,33 @@ namespace Lanzamientos_Espaciales_Exitosos
         //Método cuando la pantalla se carga
         private void Form1_Load(object sender, EventArgs e)
         {
+            //Construcción de la gráfica
+            //El área de los rectángulos es cero
+            var chart = chart1.ChartAreas[0];
+            chart.AxisX.IntervalType = DateTimeIntervalType.Number;
+            //Se pone nulo para que el numeros aparézcan en los ejes
+            chart.AxisX.LabelStyle.Format = "";
+            chart.AxisY.LabelStyle.Format = "";
+
+            chart.AxisY.LabelStyle.IsEndLabelVisible = true;
+
+            //Medidas del gráfico
+            chart.AxisX.Minimum = 1970;
+            chart.AxisX.Maximum = 2019;
+            chart.AxisX.Interval = 1;
+
+            chart.AxisY.Minimum = 0;
+            chart.AxisY.Maximum = 130;
+            chart.AxisY.Interval = 10;
+
+            //Diseño de la gráfica
+            chart1.Series.Add("Lanzamientos");
+            chart1.Series["Lanzamientos"].ChartType = SeriesChartType.Line;
+            chart1.Series["Lanzamientos"].Color = Color.Purple;
+            chart1.Series["Lanzamientos"].IsVisibleInLegend = false;
+
+
+
             //Creando nuevo objeto que recibe el path donde se encuentra el excel
             SLDocument sl = new SLDocument(path);
 
@@ -37,6 +66,8 @@ namespace Lanzamientos_Espaciales_Exitosos
 
             //El indice, para la fila, empieza en el dos
             int iRow = 2;
+
+            int año;
 
             //Creación de la lista
             List<LanzamientosViewModel> lstLanzamiento = new List<LanzamientosViewModel>();
@@ -52,6 +83,10 @@ namespace Lanzamientos_Espaciales_Exitosos
                 LanzamientosViewModel oLanzamiento = new LanzamientosViewModel();
                 oLanzamiento.Año = sl.GetCellValueAsString(iRow,1);
                 oLanzamiento.LanzamientosOrbitalesExitosos = sl.GetCellValueAsInt32(iRow, 2);
+
+                año = iRow + 1968;
+
+                chart1.Series["Lanzamientos"].Points.AddXY(año, sl.GetCellValueAsInt32(iRow,2));
 
                 //Se añade al arreglo
                 arregloLanzamientos[iRow-2] = oLanzamiento.LanzamientosOrbitalesExitosos;
@@ -76,6 +111,28 @@ namespace Lanzamientos_Espaciales_Exitosos
             double promedio = suma / arregloLanzamientos.Length;
 
             txtBoxPromedio.Text = Convert.ToString(promedio);
+
+            //Obteniendo el valor máximo y mínimo
+            int min;
+            int max;
+
+            min = max = arregloLanzamientos[0];
+
+            for (int i = 0; i < 49; i++)
+            {
+                if (min > arregloLanzamientos[i])
+                {
+                    min = arregloLanzamientos[i];
+                }
+                if (max < arregloLanzamientos[i])
+                {
+                    max = arregloLanzamientos[i];
+                }
+            }
+
+            txtBoxMax.Text = Convert.ToString(max);
+            txtBoxMin.Text = Convert.ToString(min);
+
         }
     }
 }
